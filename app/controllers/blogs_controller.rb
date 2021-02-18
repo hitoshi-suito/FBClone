d94f570967142bd05f7655d0855d6ffddc4d1f4e
hitoshi-suito <htshts153@gmail.com>
2021-02-18 17:54:06 +0900

    CRUD機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,12 @@
 class BlogsController < ApplicationController
-
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
+    @blogs = Blog.all
   end
 
   def create
-    Blog.create(content: params[:blog][:content])
+    Blog.create(blog_params)
+    redirect_to new_blog_path
   end
 
   def new
@@ -18,9 +20,24 @@ class BlogsController < ApplicationController
   end
 
   def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice:"ブログを編集しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました"
   end
 
+  private
+  def blog_params
+    params.require(:blog).permit(:content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 end
